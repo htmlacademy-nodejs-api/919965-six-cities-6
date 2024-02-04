15a866f2258627cda9f23fea1577e4f0775c6a43
aartishchev <aartishchev@yandex.ru>
2024-02-04 21:24:28 +0300

    add stream handlers

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,5 +1,7 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
+import { createOffer, getErrorMessage } from '../../shared/helpers/index.js';
+
 import chalk from 'chalk';
 
 export class ImportCommand implements Command {
@@ -7,20 +9,27 @@ export class ImportCommand implements Command {
     return '--import';
   }
 
-  public execute(...parameters: string[]): void {
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.log(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
-    try {
-      fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (error) {
-      if (!(error instanceof Error)) {
-        throw error;
-      }
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
 
+    try {
+      await fileReader.read();
+    } catch (error) {
       console.error(chalk.redBright(`Can't import data from file: ${filename}`));
-      console.error(chalk.redBright(`Details: ${error.message}`));
+      console.error(chalk.redBright(getErrorMessage(error)));
     }
   }
 }
--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,68 +1,36 @@
+import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
-import { AmenityType, CityType, HousingType, Offer, User } from '../../types/index.js';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error('File was not read');
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length)
-      .map((line) => line.split('\t'))
-      .map(
-        ([
-          name,
-          description,
-          publishDate,
-          city,
-          previewImage,
-          images,
-          isPremium,
-          isFavorite,
-          rating,
-          type,
-          roomsCount,
-          maxGuests,
-          price,
-          amenities,
-          hostName,
-          email,
-          avatarPath,
-          password,
-          userType,
-          commentsCount,
-          latitude,
-          longitude,
-        ]) => ({
-          name,
-          description,
-          publishDate: new Date(publishDate),
-          city: city as CityType,
-          previewImage,
-          images: images.split(';'),
-          isPremium: isPremium === 'true',
-          isFavorite: isFavorite === 'true',
-          rating: Number(rating),
-          type: type as HousingType,
-          roomsCount: Number(roomsCount),
-          maxGuests: Number(maxGuests),
-          price: Number(price),
-          amenities: amenities.split(';') as AmenityType[],
-          host: { name: hostName, email, avatarPath, password, type: userType } as User,
-          commentsCount: Number(commentsCount),
-          location: { latitude: Number(latitude), longitude: Number(longitude) },
-        })
-      );
+    this.emit('end', importedRowCount);
   }
 }
