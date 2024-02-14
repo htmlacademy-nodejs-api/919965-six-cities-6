60a97fe4141c078f34c3119c32cd3f113a700f56
aartishchev <aartishchev@yandex.ru>
2024-02-14 21:51:15 +0300

    add db handling

---
+++ src/shared/modules/user/user.entity.ts
@@ -0,0 +1,49 @@
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
+import { RoleType, User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class UserEntity extends defaultClasses.TimeStamps implements User {
+  @prop({ unique: true, required: true })
+  public email: string;
+
+  @prop({ required: false, default: '' })
+  public avatarPath?: string;
+
+  @prop({ required: true, default: '' })
+  public name: string;
+
+  @prop({ required: true, default: '' })
+  public password: string;
+
+  @prop({ required: true })
+  public type: RoleType;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.name = userData.name;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
+}
+
+export const UserModel = getModelForClass(UserEntity);
